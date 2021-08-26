95279f32733d0c6df83b759011dd9c7a0e9065e1
Pacifique Mucyo <mpacifique2@huskers.unl.edu>
2021-08-26 19:12:47 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Pacifique Mucyo
+ *         mpacifique2@.unl.edu
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Pacifique Mucyo!\n");
+
+  return 0;
+}
